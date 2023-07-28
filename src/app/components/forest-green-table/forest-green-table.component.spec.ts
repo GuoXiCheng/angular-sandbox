f7d789a6a53dc1adc4bf9fd162dda8bd67b26e36
Xicheng Guo <xicheng_guo@wistron.com>
2023-07-28 10:52:35 +0800

    add material

---
+++ src/app/components/forest-green-table/forest-green-table.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ForestGreenTableComponent } from './forest-green-table.component';
+
+describe('ForestGreenTableComponent', () => {
+  let component: ForestGreenTableComponent;
+  let fixture: ComponentFixture<ForestGreenTableComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ForestGreenTableComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ForestGreenTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/forest-green-table/forest-green-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forest-green-table',
+  templateUrl: './forest-green-table.component.html',
+  styleUrls: ['./forest-green-table.component.css']
+})
+export class ForestGreenTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
