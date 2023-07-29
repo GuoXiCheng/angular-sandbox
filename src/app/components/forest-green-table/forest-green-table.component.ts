f1a9a5e982a78aa63d10bc8bf83b676f107fcd54
Xicheng Guo <xicheng_guo@wistron.com>
2023-07-29 15:29:54 +0800

    update table

--- src/app/components/forest-green-table/forest-green-table.component.ts
+++ src/app/components/forest-green-table/forest-green-table.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableDataSourcePaginator, MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-forest-green-table',
   standalone: true,
   templateUrl: './forest-green-table.component.html',
-  styleUrls: ['./forest-green-table.component.css']
+  styleUrls: ['./forest-green-table.component.css'],
+  imports: [MatTableModule, MatPaginatorModule, CommonModule],
 })
-export class ForestGreenTableComponent implements OnInit {
+export class ForestGreenTableComponent<T> implements AfterViewInit {
+  @Input() columnHeader: Array<{key: string, value: string}> = []
 
-  constructor() { }
+  @Input() data: Array<T> = [];
 
-  ngOnInit() {
+  dataSource: MatTableDataSource<T, MatTableDataSourcePaginator> = new MatTableDataSource<T>(this.data);
+  displayedColumns: string[] = [];
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource<T>(this.data);
+    this.dataSource.paginator = this.paginator;
+    this.displayedColumns = this.columnHeader.map(item=>(item.key));
   }
-
 }
