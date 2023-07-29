317ee6318b47a0817d0527b461cf6d31191b3a81
Xicheng Guo <xicheng_guo@wistron.com>
2023-07-29 09:53:43 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ForestGreenTableComponent } from './components/forest-green-table/forest-green-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ForestGreenTableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/forest-green-table/forest-green-table.component.ts
+++ src/app/components/forest-green-table/forest-green-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forest-green-table',
+  standalone: true,
   templateUrl: './forest-green-table.component.html',
   styleUrls: ['./forest-green-table.component.css']
 })
