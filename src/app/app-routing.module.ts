1ed3970841a67588fe96466696a458c3487dcabc
GuoXiCheng <1377994267@qq.com>
2023-08-03 22:39:20 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './pages/first-page/first-page.component';
+import { SecondPageComponent } from './pages/second-page/second-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstPageComponent },
+  { path: 'first', component: FirstPageComponent },
+  { path: 'second', component: SecondPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
