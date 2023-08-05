a2edf460b31cf3c8a75648f9d1110a737fe7c6cd
GuoXiCheng <1377994267@qq.com>
2023-08-05 22:27:55 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit, Renderer2 } from "@angular/core";
-
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
-  })
-  export class AppComponent implements OnInit {
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
 
-    isDarkTheme = false;
+  isDarkTheme = false;
 
-    constructor(private renderer: Renderer2) {
-  
-    }
-    ngOnInit(): void {
-    }
+  constructor(private renderer: Renderer2) {
+  }
+  ngOnInit(): void {
 
-    toggleTheme() {
-      this.isDarkTheme = !this.isDarkTheme;
-      if (this.isDarkTheme) {
-        this.renderer.removeClass(document.body, 'light-theme');
-        this.renderer.addClass(document.body, 'dark-theme');
-      } else {
-        this.renderer.removeClass(document.body, 'dark-theme');
-        this.renderer.addClass(document.body, 'light-theme');
-      }
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    if (this.isDarkTheme) {
+      this.renderer.removeClass(document.body, 'light-theme');
+      this.renderer.addClass(document.body, 'dark-theme');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-theme');
+      this.renderer.addClass(document.body, 'light-theme');
     }
+  }
 }
