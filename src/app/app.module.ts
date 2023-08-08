b524108c069cd8d0f7a64ba8898d37dfd462c55c
Xicheng Guo <xicheng_guo@wistron.com>
2023-08-08 09:38:18 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,12 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { MatSnackbarComponent } from './components/mat-snackbar/mat-snackbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NotificationRuleDialogComponent } from './components/notification-rule-dialog/notification-rule-dialog.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,6 +55,8 @@ import { NotificationRuleDialogComponent } from './components/notification-rule-
     NotificationRuleDialogComponent
   ],
   providers: [
+  
+    { provide: NZ_I18N, useValue: zh_CN }
   ],
   bootstrap: [AppComponent]
 })
