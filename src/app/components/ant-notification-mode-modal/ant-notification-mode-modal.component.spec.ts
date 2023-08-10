0201f21680f9856e54437547e59d12a09bab487d
Xicheng Guo <xicheng_guo@wistron.com>
2023-08-10 10:09:36 +0800

    update form

--- src/app/components/ant-notification-mode-modal/ant-notification-mode-modal.component.spec.ts
+++ src/app/components/ant-notification-mode-modal/ant-notification-mode-modal.component.spec.ts
@@ -1,28 +1,41 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { AntNotificationModeModalComponent } from './ant-notification-mode-modal.component';
 
-// import { AntNotificationModeModalComponent } from './ant-notification-mode-modal.component';
+describe('AntNotificationModeModalComponent', () => {
+  let component: AntNotificationModeModalComponent;
+  let fixture: ComponentFixture<AntNotificationModeModalComponent>;
 
-// describe('AntNotificationModeModalComponent', () => {
-//   let component: AntNotificationModeModalComponent;
-//   let fixture: ComponentFixture<AntNotificationModeModalComponent>;
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [ReactiveFormsModule, NzModalModule, BrowserAnimationsModule,AntNotificationModeModalComponent]
+    }).compileComponents();
+  }));
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AntNotificationModeModalComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AntNotificationModeModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AntNotificationModeModalComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  // Add more test cases here based on your component's functionality
+  // For example, test modal visibility, form initialization, form controls behavior, etc.
+
+  it('should disable submit button when form is invalid', () => {
+    component.parentForm.controls['notificationMode'].setValue('email');
+    component.parentForm.controls['receiverName'].setValue('Test Receiver');
+    component.parentForm.controls['emailFrom'].setValue('invalid-email'); // Invalid email
+
+    fixture.detectChanges();
+
+    expect(component.nzOkDisabled).toBe(true);
+  });
+
+});
--- src/app/components/ant-notification-mode-modal/ant-notification-mode-modal.component.ts
+++ src/app/components/ant-notification-mode-modal/ant-notification-mode-modal.component.ts
@@ -6,14 +6,23 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
-import { Observable, Observer, Subscription } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 export interface EmailForm {
   notificationMode: 'email';
   receiverName: string;
-  emailFrom: string | undefined;
+  emailFrom?: string;
   emailTo: string;
   sendResolved: string;
+}
+
+export interface EmailEntity {
+  id?: number;
+  receiver_name: string;
+  receiver_type: 'email';
+  email_from?: string;
+  email_to: string;
+  send_resolved: boolean;
 }
 
 export interface TeamsForm {
@@ -21,6 +30,14 @@ export interface TeamsForm {
   receiverName: string;
   teamsUrl: string;
   sendResolved: string;
+}
+
+export interface TeamsEntity {
+  id?: number;
+  receiver_name: string;
+  receiver_type: 'teams';
+  web_hook_url: string;
+  send_resolved: boolean;
 }
 
 @Component({
@@ -33,27 +50,35 @@ export interface TeamsForm {
 export class AntNotificationModeModalComponent implements OnInit {
 
   @Input() isVisible = false;
+  @Input() formData: EmailEntity | TeamsEntity | null = null;
 
   @Output() isVisibleChange = new EventEmitter<boolean>();
   @Output() formValueChange = new EventEmitter<any>();
 
 
   nzOkDisabled = true;
-
+  nzDisabledSelect = false;
   parentForm!: UntypedFormGroup;
-  valueChangesSubscription!: Subscription;
 
   constructor(private fb: UntypedFormBuilder) {
 
   }
 
   ngOnInit() {
+
     this.parentForm = this.fb.group({});
-    this.handleNotificationModeChange('email');
+
+    if (this.formData === null) {
+      this.handleNotificationModeChange('email');
+      this.nzDisabledSelect = false;
+    } else {
+      this.handleNotificationModeChange(this.formData.receiver_type, this.formData);
+      this.nzDisabledSelect = true;
+    }
 
     this.parentForm.statusChanges.subscribe(() => {
       this.nzOkDisabled = !this.parentForm.valid;
-    })
+    });
   }
 
   handleCancel() {
@@ -68,6 +93,7 @@ export class AntNotificationModeModalComponent implements OnInit {
     switch (formValue.notificationMode) {
       case 'email':
         return {
+          id: this.formData?.id,
           receiver_type: 'email',
           email_from: formValue.emailFrom,
           email_to: formValue.emailTo,
@@ -75,6 +101,7 @@ export class AntNotificationModeModalComponent implements OnInit {
         };
       case 'teams':
         return {
+          id: this.formData?.id,
           receiver_type: 'teams',
           webhook_url: formValue.teamsUrl,
           send_resolved: formValue.sendResolved === 'true'
@@ -84,23 +111,23 @@ export class AntNotificationModeModalComponent implements OnInit {
     }
   }
 
-  handleNotificationModeChange(mode: string) {
+  handleNotificationModeChange(mode: string, entity?: EmailEntity | TeamsEntity) {
     // 先删除当前 FormGroup 中的所有控件
     Object.keys(this.parentForm.controls).forEach(key => {
       this.parentForm.removeControl(key);
     });
 
     this.parentForm.addControl('notificationMode', this.fb.control(mode, [Validators.required]));
-    this.parentForm.addControl('receiverName', this.fb.control('', [Validators.required]));
-    this.parentForm.addControl('sendResolved', this.fb.control('true', [Validators.required]));
+    this.parentForm.addControl('receiverName', this.fb.control(entity?.receiver_name, [Validators.required]));
+    this.parentForm.addControl('sendResolved', this.fb.control(entity ? entity.send_resolved.toString() : 'true', [Validators.required]));
 
     switch (mode) {
       case 'email':
-        this.parentForm.addControl('emailFrom', this.fb.control('', Validators.compose([Validators.required]), this.emailFromAsyncValidator as AsyncValidatorFn));
-        this.parentForm.addControl('emailTo', this.fb.control('', Validators.compose([Validators.required]), this.emailToAsyncValidator as AsyncValidatorFn));
+        this.parentForm.addControl('emailFrom', this.fb.control((entity as EmailEntity)?.email_from, Validators.compose([Validators.required]), this.emailFromAsyncValidator as AsyncValidatorFn));
+        this.parentForm.addControl('emailTo', this.fb.control((entity as EmailEntity)?.email_to, Validators.compose([Validators.required]), this.emailToAsyncValidator as AsyncValidatorFn));
         break;
       case 'teams':
-        this.parentForm.addControl('teamsUrl', this.fb.control('', [Validators.required], this.teamsUrlAsyncValidator as AsyncValidatorFn));
+        this.parentForm.addControl('teamsUrl', this.fb.control((entity as TeamsEntity)?.web_hook_url, [Validators.required], this.teamsUrlAsyncValidator as AsyncValidatorFn));
         break;
     }
   }
@@ -111,6 +138,7 @@ export class AntNotificationModeModalComponent implements OnInit {
         const urlPattern = /^https:\/\//;
         urlPattern.test(control.value) ? observer.next(null) : observer.next({ error: true });
         observer.complete();
+        this.nzOkDisabled = !this.parentForm.valid;
       }, 1000);
     });
 
@@ -121,6 +149,7 @@ export class AntNotificationModeModalComponent implements OnInit {
         const temp = control.value.split(',') as string[];
         temp.every(ele => emailRegex.test(ele)) ? observer.next(null) : observer.next({ error: true });
         observer.complete();
+        this.nzOkDisabled = !this.parentForm.valid;
       }, 1000);
     });
 
@@ -130,6 +159,7 @@ export class AntNotificationModeModalComponent implements OnInit {
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         emailRegex.test(control.value) ? observer.next(null) : observer.next({ error: true });
         observer.complete();
+        this.nzOkDisabled = !this.parentForm.valid;
       }, 1000);
     });
 
