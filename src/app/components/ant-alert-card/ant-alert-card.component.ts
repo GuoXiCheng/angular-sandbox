1392d09516276c1df2e27d2535562d4674e3cd10
Xicheng Guo <xicheng_guo@wistron.com>
2023-08-15 14:13:43 +0800

    update card

--- src/app/components/ant-alert-card/ant-alert-card.component.ts
+++ src/app/components/ant-alert-card/ant-alert-card.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
@@ -7,9 +8,24 @@ export interface AlertCard {
   summary: string;
   severity: 'low' | 'medium' | 'high' | 'critical';
   status: 'firing' | 'resolved';
-  start_at: string;
+  starts_at: string;
   duration: string;
-  alertname: string
+  alertname: string;
+  description: string;
+}
+
+export interface AlertRecordEntity {
+  id: number;
+  serial: string;
+  alertname: string;
+  severity: 'low' | 'medium' | 'high' | 'critical';
+  owner: string;
+  status: 'firing' | 'resolved';
+  starts_at: string;
+  ends_at: string;
+  fingerprint: string;
+  summary: string | null;
+  description: string | null;
 }
 
 @Component({
@@ -21,11 +37,34 @@ export interface AlertCard {
 })
 export class AntAlertCardComponent implements OnInit {
 
-  @Input() cardData!: AlertCard;
+  cardData!: AlertCard;
 
-  constructor() { }
+  @Input() data!: AlertRecordEntity;
+
+  constructor() {
+    
+  }
 
   ngOnInit() {
+  
+    const {severity, status, starts_at, ends_at, alertname, summary, description} = this.data;
+
+    const startsAt = moment(starts_at);
+    const endsAt = moment(ends_at);
+    const realEndsAt = endsAt.isAfter(startsAt) ? endsAt: moment();
+
+    const duration = moment.duration(realEndsAt.diff(startsAt));
+    const formattedDuration = `${duration.days()}d ${duration.hours()}h ${duration.minutes()}m`;
+
+    this.cardData = {
+      alertname,
+      severity,
+      status,
+      starts_at: moment(starts_at).format("YYYY-MM-DD HH:mm:ss"),
+      duration: formattedDuration,
+      summary: summary? summary: 'NO TITLE',
+      description: description? description: 'NO DESCRIPTION'
+    }
   }
 
 }
