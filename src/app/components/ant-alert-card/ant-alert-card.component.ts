4e90335e81283c02dd78b4d0e4ea44574bd95ad4
Xicheng Guo <xicheng_guo@wistron.com>
2023-08-15 11:01:07 +0800

    update card

--- src/app/components/ant-alert-card/ant-alert-card.component.ts
+++ src/app/components/ant-alert-card/ant-alert-card.component.ts
@@ -3,6 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
+export interface AlertCard {
+  summary: string;
+  severity: 'low' | 'medium' | 'high' | 'critical';
+  status: 'firing' | 'resolved';
+  start_at: string;
+  duration: string;
+  alertname: string
+}
+
 @Component({
   selector: 'app-ant-alert-card',
   templateUrl: './ant-alert-card.component.html',
@@ -12,7 +21,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 })
 export class AntAlertCardComponent implements OnInit {
 
-  @Input() severity = 'low';
+  @Input() cardData!: AlertCard;
 
   constructor() { }
 
--- src/app/pages/fifth-page/fifth-page.component.ts
+++ src/app/pages/fifth-page/fifth-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertCard } from 'src/app/components/ant-alert-card/ant-alert-card.component';
 
 @Component({
   selector: 'app-fifth-page',
@@ -7,14 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FifthPageComponent implements OnInit {
 
-  cardList = [{
-    severity: 'low'
+  cardList: AlertCard[] = [{
+    summary: 'First occurrence of GDCEmailAlert',
+    severity: 'low',
+    status: 'firing',
+    alertname: 'GDCEmailAlert',
+    start_at: '2021-01-20T10:00:00.000Z',
+    duration: '1d 12h 12min'
   }, {
-    severity: 'medium'
+    summary: 'Third occurrence of GDCEmailAlert',
+    severity: 'medium',
+    status: 'resolved',
+    alertname: 'GDCEmailAlert',
+    start_at: '2021-01-20T10:00:00.000Z',
+    duration: '1d 12h 12min'
   }, {
-    severity: 'high'
+    summary: 'The pipeline with project name px-web-app failed to run.',
+    severity: 'high',
+    status: 'firing',
+    alertname: 'GDCEmailAlert',
+    start_at: '2021-01-20T10:00:00.000Z',
+    duration: '1d 12h 12min'
   }, {
-    severity: 'critical'
+    summary: 'Second occurrence of GDCEmailAlert',
+    severity: 'critical',
+    status: 'resolved',
+    alertname: 'GDCEmailAlert',
+    start_at: '2021-01-20T10:00:00.000Z',
+    duration: '1d 12h 12min'
   }]
 
   constructor() { }
