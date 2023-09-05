b8058a148108ca1763c0cc2e8caa79a771889c24
Xicheng Guo <xicheng_guo@wistron.com>
2023-09-05 10:30:02 +0800

    update ant-side-bar

--- src/app/pages/fifth-page/fifth-page.component.ts
+++ src/app/pages/fifth-page/fifth-page.component.ts
@@ -11,13 +11,27 @@ export class FifthPageComponent implements OnInit {
 	constructor() { }
 
 	dataset = [
-		{ date: '2023-08-29', exceptionNum: 10 },
-		{ date: '2023-08-30', exceptionNum: 99 },
-		{ date: '2023-08-31', exceptionNum: 156 },
-		{ date: '2023-09-01', exceptionNum: 12365 },
-		{ date: '2023-09-02', exceptionNum: 489 },
-		{ date: '2023-09-03', exceptionNum: 0 },
-		{ date: '2023-09-04', exceptionNum: 1 },
+		{
+			date: '2023-08-30', openNum: 99, closedNum: 100
+		},
+		{
+			date: '2023-08-31', openNum: 156, closedNum: 1000
+		},
+		{
+			date: '2023-09-01', openNum: 12365, closedNum: 0
+		},
+		{
+			date: '2023-09-02', openNum: 489, closedNum: 10
+		},
+		{
+			date: '2023-09-03', openNum: 0, closedNum: 0
+		},
+		{
+			date: '2023-09-04', openNum: 1, closedNum: 10
+		},
+		{
+			date: '2023-09-05', openNum: 10, closedNum: 10
+		}
 	]
 
 	ngOnInit() {
@@ -30,7 +44,8 @@ export class FifthPageComponent implements OnInit {
 	handleUpEvent(date: Dayjs) {
 		const result = Array.from({ length: 7 }, (_, index) => index + 1).map((item) => ({
 			date: date.clone().subtract(item - 1, 'day').format('YYYY-MM-DD'),
-			exceptionNum: Math.floor(Math.random() * 100)
+			openNum: Math.floor(Math.random() * 100),
+			closedNum: Math.floor(Math.random() * 100)
 		}));
 		this.dataset = result.reverse();
 	}
@@ -38,8 +53,11 @@ export class FifthPageComponent implements OnInit {
 	handleDownEvent(date: Dayjs) {
 		const result = Array.from({ length: 7 }, (_, index) => index + 1).map((item) => ({
 			date: date.clone().subtract(item - 1, 'day').format('YYYY-MM-DD'),
-			exceptionNum: Math.floor(Math.random() * 100)
+			openNum: Math.floor(Math.random() * 100),
+			closedNum: Math.floor(Math.random() * 100)
 		}));
 		this.dataset = result.reverse();
 	}
+
+
 }
