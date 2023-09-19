f8c6dfaa9d63c6be8aa7113545a0ecad4100181a
Xicheng Guo <xicheng_guo@wistron.com>
2023-09-19 13:26:12 +0800

    add toolbar

---
+++ src/app/pages/eighth-page/eighth-page.component.spec.ts
@@ -0,0 +1,28 @@
+// /* tslint:disable:no-unused-variable */
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { By } from '@angular/platform-browser';
+// import { DebugElement } from '@angular/core';
+
+// import { EighthPageComponent } from './eighth-page.component';
+
+// describe('EighthPageComponent', () => {
+//   let component: EighthPageComponent;
+//   let fixture: ComponentFixture<EighthPageComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ EighthPageComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(EighthPageComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
